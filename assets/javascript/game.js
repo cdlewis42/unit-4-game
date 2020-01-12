8c02f29f243f528da2ccf1bfc7c2d033eeb4b481
Celeste Lewis <cdlewis42@gmail.com>
2020-01-12 11:22:07 -0500

    got the random number to display/simplified random number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,24 @@
 
 //Get the number to guess
-function getNumber(){
-    var numberToGuess=(Math.floor(Math.random() * 121))
-    console.log(numberToGuess)
-        if (numberToGuess<19){
-            numberToGuess = 19
-            console.log(numberToGuess)
-        }
-    return numberToGuess
-}
+var numberToGuess=(Math.floor(Math.random() * 101+19))
+
 //NEED TO FIX: display number to guess
-function displayNumber(num){
-    $("number").text(num);
-}
+$("#number").text(numberToGuess)
 
 // TO DO: Assign random values to all 4 buttons with for loop.
-function buttonValue{
+function buttonValue(){
+    var value=$(".button").val((Math.floor(Math.random() * 12)))
+    if(value<1){
+        value=1;
+    }
+    console.log(value)
 
 }
 //TO DO: When button is clicked adds value to score
 //TO DO: if score matches numberTOGUESS player wins
 //TO DO if score>numberToGuess player loses
 
-//TO DO:Call function to either match or lose score every time player clicks
+
 getNumber()
 
-displayNumber(numberToGuess)
+buttonValue()
