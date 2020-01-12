9cca9fe66f08ab0940717646ac926d13e11d15e6
Celeste Lewis <cdlewis42@gmail.com>
2020-01-12 11:37:24 -0500

    added variables/display for wins/losses/score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,24 @@
-
+//add variables for win/loss/score
+var win=0;
+var loss=0;
+var score=0;
+//display the zeros for the variables.
+$("#wins").append(win);
+$("#losses").append(loss);
+$("#score").append(score);
 //Get the number to guess
 var numberToGuess=(Math.floor(Math.random() * 101+19))
 
-//NEED TO FIX: display number to guess
+//display number to guess
 $("#number").text(numberToGuess)
 
-// TO DO: Assign random values to all 4 buttons with for loop.
-function buttonValue(){
-    var value=$(".button").val((Math.floor(Math.random() * 12)))
-    if(value<1){
-        value=1;
-    }
-    console.log(value)
 
-}
+//Assign random values to all 4 buttons.
+
+var em1=(Math.floor(Math.random() * 11+1))
+var em2=(Math.floor(Math.random() * 11+1))
+var em3=(Math.floor(Math.random() * 11+1))
+var em4=(Math.floor(Math.random() * 11+1))
 //TO DO: When button is clicked adds value to score
 //TO DO: if score matches numberTOGUESS player wins
 //TO DO if score>numberToGuess player loses
