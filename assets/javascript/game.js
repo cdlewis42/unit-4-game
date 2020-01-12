f5c931d26c630032a120878da97be62e289ddf21
Celeste Lewis <cdlewis42@gmail.com>
2020-01-12 12:08:38 -0500

    made values appear once clicked on.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,6 +29,11 @@ function resetGame(){
     em4=(Math.floor(Math.random() * 11+1));
     numberToGuess=(Math.floor(Math.random() * 101+19));
     $("#number").text(numberToGuess)
+    $("#button1").html("<img src='assets/images/blue-emerald.jpg'>");
+    $("#button2").html("<img src='assets/images/green emerald.jpg'>");
+    $("#button3").html("<img src='assets/images/purple-emerald.jfif'>");
+    $("#button4").html("<img src='assets/images/red-emerald.jpg'>");
+
 }
 
 //the functions for if score matches numberToGuess player wins or loses
@@ -49,6 +54,9 @@ function playerLose(){
 
 $("#button1").on("click",function(){
     score=score+em1;
+    //changes button to show number value.
+    $("#button1").html("<img src='assets/images/blue-emerald.jpg'><h2>"+em1+"<h2>");
+
     $("#score").text(score);
         if (score>numberToGuess){
             playerLose()
@@ -57,8 +65,10 @@ $("#button1").on("click",function(){
             playerWin();
         }
 })
+
 $("#button2").on("click",function(){
     score=score+em2;
+    $("#button2").html("<img src='assets/images/green emerald.jpg'><h2>"+em2+"<h2>")
     $("#score").text(score);
         if (score>numberToGuess){
             playerLose()
@@ -67,8 +77,10 @@ $("#button2").on("click",function(){
             playerWin();
         }
 })
+
 $("#button3").on("click",function(){
     score=score+em3;
+    $("#button3").html("<img src='assets/images/purple-emerald.jfif'><h2>"+em3+"<h2>")
     $("#score").text(score);
         if (score>numberToGuess){
             playerLose()
@@ -77,8 +89,10 @@ $("#button3").on("click",function(){
             playerWin();
         }
 })
+
 $("#button4").on("click",function(){
     score=score+em4;
+    $("#button4").html("<img src='assets/images/red-emerald.jpg'><h2>"+em4+"<h2>")
     $("#score").text(score);
         if (score>numberToGuess){
             playerLose()
